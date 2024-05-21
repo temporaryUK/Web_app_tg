142eb15766b8fb86cf77ef21e1bcf8a9c514afa2
temporaryUK <nodrefresh@gmail.com>
2024-05-21 15:08:49 +0300

    Init commit

--- app/keyboards.py
+++ app/keyboards.py
@@ -2,6 +2,7 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
+
 # main = ReplyKeyboardMarkup(keyboard=[
 #     [KeyboardButton(text="Каталог"), KeyboardButton(text="Контакты")],
 #     [KeyboardButton(text="Корзина")]
